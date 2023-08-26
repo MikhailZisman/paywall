e4b570559978a14baa74aede97ef211a0f26de9d
MikhailZisman <36883470+MikhailZisman@users.noreply.github.com>
2023-08-26 13:28:01 +0400

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+  repositories {
+    gradlePluginPortal()
+    google()
+    mavenCentral()
+  }
+}
+
+plugins {
+  id("com.gradle.enterprise") version "3.13"
+}
+
+enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
+
+rootProject.name = "template-kmp-library"
+includeBuild("./build-conventions/")
+include(":tests:test-utils")
+
+include(
+  ":modules:template-kmp-library-core",
+  ":tests:template-kmp-library-core-tests"
+)
+
+include(
+  ":modules:template-kmp-library-dsl",
+  ":tests:template-kmp-library-dsl-tests"
+)
